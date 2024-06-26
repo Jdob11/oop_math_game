0f832ef72b6f8ed4e87196f7baf68e1d132e5d12
Jdob11 <jbdobson11@gmail.com>
2024-06-26 10:59:18 -0700

    feat: Add command line prompts for players names

--- main.rb
+++ main.rb
@@ -3,5 +3,13 @@ require './player'
 require './question'
 require './turn'
 
-game = Game.new("Player 1", "Player 2")
+# Prompt for player names
+print "Enter name for Player 1: "
+player1_name = gets.chomp
+
+print "Enter name for Player 2: "
+player2_name = gets.chomp
+
+# Initialize and start the game
+game = Game.new(player1_name, player2_name)
 game.start_game
