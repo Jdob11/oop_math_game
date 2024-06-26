4612be2b4d1ddada8eeee0f9b917b9b5566bb7a2
Jdob11 <jbdobson11@gmail.com>
2024-06-25 17:05:37 -0700

    feat: Start game class implementation

---
+++ game.rb
@@ -0,0 +1,24 @@
+class Game
+  def initialize(player1_name, player2_name)
+    @player1 = player1_name
+    @player2 = player2_name
+    @current_player = @player1
+    @game_over = false
+  end
+
+  def start_game
+
+  end
+
+  def switch_turns
+
+  end
+
+  def check_game_over
+
+  end
+
+  def announce_winner
+
+  end
+end
